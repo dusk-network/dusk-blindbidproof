50a62aa868b1dbf294e0eb9e98f081a429878197
BlockChainDev <kevtheappdev@gmail.com>
2019-03-01 12:11:27 +0000

    - Added tests - renamed main_test to bidproof_test

--- go/bidproof_test.go
+++ go/bidproof_test.go
@@ -4,7 +4,27 @@ import (
 	"testing"
 
 	ristretto "github.com/bwesterb/go-ristretto"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestProveVerify(t *testing.T) {
+
+	for n := 0; n < 20; n++ {
+		d := genRandScalar()
+		k := genRandScalar()
+		seed := genRandScalar()
+
+		// public list of bids
+		pubList := make([]ristretto.Scalar, 0, 5)
+		for i := 0; i < 5; i++ {
+			pubList = append(pubList, genRandScalar())
+		}
+
+		proof, qBytes, zBytes, pL := Prove(d, k, seed, pubList)
+		res := Verify(proof, seed.Bytes(), pL, qBytes, zBytes)
+		assert.Equal(t, true, res)
+	}
+}
 
 func BenchmarkProveVerify(b *testing.B) {
 
