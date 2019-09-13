ccba038dc7413f5c7b7b3120864770a0fd3b64e8
Victor Lopez <victor@dusk.network>
2019-09-13 13:55:34 +0200

    Update documentation

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["bulletproofs"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"bulletproofs/enum.ProofError.html\" title=\"enum bulletproofs::ProofError\">ProofError</a>",synthetic:false,types:["bulletproofs::errors::ProofError"]},{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"bulletproofs/range_proof_mpc/enum.MPCError.html\" title=\"enum bulletproofs::range_proof_mpc::MPCError\">MPCError</a>",synthetic:false,types:["bulletproofs::errors::MPCError"]},{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"bulletproofs/r1cs/enum.R1CSError.html\" title=\"enum bulletproofs::r1cs::R1CSError\">R1CSError</a>",synthetic:false,types:["bulletproofs::errors::R1CSError"]},];
+implementors["failure"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
